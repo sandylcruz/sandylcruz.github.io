b3d35acf6382ee80558d35bcd76e1f776f0a7502
Sandy Cruz <sandyleahcruz@icloud.com>
2021-08-18 21:52:23 -0700

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-7de69ce510e05d044333.js"
+    "url": "webpack-runtime-5f660614dd65e908f6ee.js"
   },
   {
     "url": "framework-dbb498007a7447f28d8e.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "988f22f4ccb7ef463b9abdc6b2e222fa"
+    "revision": "8d12e06530a8a790966a61ebf52e6a8f"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
